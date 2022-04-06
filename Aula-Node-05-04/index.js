49d5f4298df8f47286d903e372f36088486e601f
Allas Assis de Oliveira <88514585+allasassis@users.noreply.github.com>
2022-04-05 21:32:43 -0300

    Upei os arquivos

---
+++ Aula-Node-05-04/index.js
@@ -0,0 +1,8 @@
+const{createServer} = require('http')
+let server = createServer((request,response)=> {
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write('<h1>Hello World!</h1>');
+    response.end();
+});
+server.listen(8000);
+console.log("Servidor Iniciado!");
