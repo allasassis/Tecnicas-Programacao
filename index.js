c9f75d22e3e4076e73ccdaf11c79b2e0cc9f1f94
Allas Assis de Oliveira <88514585+allasassis@users.noreply.github.com>
2022-04-26 21:19:02 -0300

    Upei os arquivos
    USAR O NPM EXPRESS PARA INSTALAR OS MÓDULOS QUE NÃO DEU PARA UPAR NO GIT

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("Projeto Node - Aula de Técnicas de Programação!");
-console.log("Uma alteração no arquivo.");
+// TODOS OS CÓDIGOS E EXPLICAÇÕES NO SLIDE NODE.JS
+
+/* const{createServer} = require('http');
+let server = createServer((request,response)=>{
+    response.writeHead(200,{"Content_Type":"text/html"});
+    response.write('<h1> Hello World! </h1><p> Projeto</p>');
+    response.end();
+});
+server.listen(8000);
+console.log("Projeto iniciado com sucesso!"); */
+
+let express = require('express');
+let app = express();
+
+app.get('/',function(req, res){
+    res.send('ROTA GET')
+});
+
+app.get('/rota1',function(req, res){
+    res.send('Você está acessando a rota 1! ');
+});
+
+app.listen(3000, function(){
+    console.log("Projeto iniciado na porta 3000");
+});
